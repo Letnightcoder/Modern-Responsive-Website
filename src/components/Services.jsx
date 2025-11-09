1b986773a7dc91061a7f482fb49637abd358d01f
Official-Kallol <official.kallol2024@gmail.com>
2025-11-10 03:01:32 +0530

    Add Services section with ServiceCard and Title components; update index.html for smooth scrolling

---
+++ src/components/Services.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import assets from "../assets/assets";
+import Title from "./Title";
+import ServiceCard from "./ServiceCard";
+
+const Services = () => {
+  const servicesData = [
+    {
+      title: "Advertising",
+      description:
+        "we turn bold ideas into powerful digital solutions that connect, engage...",
+      icon: assets.ads_icon,
+    },
+    {
+      title: "Content marketing",
+      description:
+        "We help you execute your plan solutions that connect and deliver results.",
+      icon: assets.marketing_icon,
+    },
+    {
+      title: "Content writing",
+      description:
+        "We help you create a marketing strategy with digital solutions that drives results.",
+      icon: assets.content_icon,
+    },
+    {
+      title: "Social media",
+      description:
+        "We help you build a strong social media presence and engage with your audience.",
+      icon: assets.social_icon,
+    },
+  ];
+
+  return (
+    <div
+      id="services"
+      className="relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white"
+    >
+      <img
+        src={assets.bgImage2}
+        className="absolute -top-110 -left-70 -z-1 dark:hidden"
+        alt="bgImg"
+      />
+      <Title
+        title="How can we help?"
+        desc="From strategy to execution, we craft digital solutions that move your business forward."
+      />
+      <div className="flex flex-col md:grid grid-cols-2">
+        {servicesData.map((service, id) => {
+          return <ServiceCard key={id} service={service} id={id} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
