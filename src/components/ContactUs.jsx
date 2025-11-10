6d4a9f1cc0d659d685acf61430cc3fdc0f74ec63
Official-Kallol <official.kallol2024@gmail.com>
2025-11-10 13:07:23 +0530

    Add Footer component for enhanced site navigation and subscription; update ContactUs and OurWork IDs for consistency

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -31,7 +31,7 @@ const ContactUs = () => {
 
   return (
     <div
-      id="Contact-us"
+      id="contact-us"
       className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white"
     >
       <Title
